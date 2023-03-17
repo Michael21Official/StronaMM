e8a14431718a508c92469c6033aedeba2188835f
Michał Matsalak <115347711+Michael21Official@users.noreply.github.com>
2023-03-17 23:29:02 +0100

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -28,6 +28,7 @@ var Layout = function () {
         }
         $(window).scroll(function() {
             if ($('.navbar').offset().top > 150) {
+                
                 $('.navbar-fixed-top').addClass('top-nav-collapse');
             } else {
                 $('.navbar-fixed-top').removeClass('top-nav-collapse');
